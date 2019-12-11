335d4cfe692efd4f401f7b42fd2dd074268ae952
Gert Vermeersch <vermeersch.gert@gmail.com>
2019-12-12 00:51:54 +0100

    voltage reading and charge bar

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,45 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 
+#define VOLTAGE_PIN A0
 
+double current_voltage = 12.1;
 
 U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, 12, 11, 10); //SCK = 12, MOSI =11, CS 10
 
 void setup();
 void draw();
 void loop();
+void drawLayout();
+void drawParams();
 
 void setup() {
+  pinMode(VOLTAGE_PIN, INPUT);
   u8g2.setFontRefHeightExtendedText();
   u8g2.begin();
+  Serial.begin(9600);
+  Serial.println("SolarBMS v0.1");
 
+  
+}
 
+void loop() {
+  current_voltage = analogRead(VOLTAGE_PIN) * 0.0048828125 * 3;
+  Serial.println(current_voltage);
   //draw
   u8g2.firstPage();
   do {
     draw();
   } while ( u8g2.nextPage() );
-}
-
-void loop() {
-  
+  delay(1000);
 }
 
 void draw() {
+  drawLayout();
+  drawParams();
+}
+
+void drawLayout() {
   u8g2.setFont(u8g_font_helvR08);
   u8g2.setFontPosTop();
   /*u8g.drawStr( 3, 50, "Outlets");
@@ -39,14 +53,14 @@ void draw() {
   u8g.drawFrame(0, 48, 128, 16);*/
 
   //first params
-  u8g2.drawStr(2, 2, "Volt: 12.5V");
-  u8g2.drawStr(2, 12, "Amp: 1.25A"); //add 10 px
-  u8g2.drawStr(2, 22, "Watt: 15.6W");
+  u8g2.drawStr(2, 2, "Volt:");
+  u8g2.drawStr(2, 12, "Amp: "); //add 10 px
+  u8g2.drawStr(2, 22, "Watt: ");
 
   //amps params
-  u8g2.drawStr(65, 2, "Ah/1h: 5");
-  u8g2.drawStr(65, 12, "Ah/12h: 12"); //add 10 px
-  u8g2.drawStr(65, 22, "Ah/24h: 14");
+  u8g2.drawStr(65, 2, "Ah/1h:");
+  u8g2.drawStr(65, 12, "Ah/12h:"); //add 10 px
+  u8g2.drawStr(65, 22, "Ah/24h:");
 
   u8g2.drawFrame(0, 0, 128, 34);
 
@@ -58,17 +72,21 @@ void draw() {
   //u8g2.drawDisc(30, 42, 4);
 
   //charge bar
+  int bar_progress = current_voltage * 8.85;
   u8g2.drawFrame(0, 50, 128, 14); //charge frame
-  u8g2.drawBox(2, 52, 124, 10); //charge bar (0 = empty, 124 = full)
+  u8g2.drawBox(2, 52, bar_progress, 10); //charge bar (0 = empty, 124 = full)
   
 
   
 
   u8g2.setFont(u8g_font_helvR08);
   u8g2.setFontPosTop();
+}
 
- 
-
- 
-
+void drawParams() {
+  char buf[6];
+  char voltage[7];
+  dtostrf(current_voltage, 4, 2, buf);
+  snprintf(voltage, sizeof voltage, "%sV", buf);
+  u8g2.drawStr(25,2, voltage);
 }
